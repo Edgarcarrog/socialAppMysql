54c918cdabe269d153b01093b8c23d7c5ba9f9cb
Edgarcarrog <edgc21@hotmail.com>
2022-09-01 17:03:53 -0500

    HomePage's sendPost implemented

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,12 +1,14 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import clienteAxios from "../config/axios";
 import { context } from "../context/context";
 import { getCookie } from "../helpers/cookie";
 
 const HomePage = () => {
-  const { addUser, setAllUsers, setFollowers, setFollowing } =
+  const { user, addUser, setAllUsers, setFollowers, setFollowing } =
     useContext(context);
+
+  const [description, setDescription] = useState("");
 
   const setUsers = async () => {
     const user = getCookie("user");
@@ -23,19 +25,40 @@ const HomePage = () => {
     setFollowers(followers.data.data);
   };
 
+  const sendPost = async (description) => {
+    console.log("pasó por sendPost", description);
+    await clienteAxios.post(`/posts/${user.userId}`, { description });
+  };
+
+  const handleChange = (e) => {
+    setDescription(e.target.value);
+  };
+
   useEffect(() => {
     try {
       setUsers();
     } catch (error) {
       console.log(error.message);
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <div>
       <h1>Posts</h1>
-      <button className="btn">Crear Post</button>
+      <div className="post__container">
+        <textarea
+          className="post__area"
+          name="message"
+          rows="5"
+          placeholder="Comparte un mensaje"
+          required
+          onChange={handleChange}
+        />
+        <button className="btn" onClick={() => sendPost(description)}>
+          Crear Post
+        </button>
+      </div>
       <Link className="btn" to="/profile">
         Perfil
       </Link>
