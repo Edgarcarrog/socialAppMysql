0c5be2c7853b74b5103c65706ad4db0f8c090de3
Edgarcarrog <56626783+Edgarcarrog@users.noreply.github.com>
2023-07-22 04:36:25 -0600

    console req.header

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -34,6 +34,7 @@ exports.updateUser = async (req, res) => {
 
 exports.verifyUser = async (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
+  console.log(req.header);
   const result = await userService.verifyUser(req.params.user);
   res.status(result.status).json({ message: result.msg, data: result.data });
 };
