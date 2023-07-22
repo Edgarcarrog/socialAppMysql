7fa6c2f09e73ef56bf413285c519583f1e1d2615
Edgarcarrog <56626783+Edgarcarrog@users.noreply.github.com>
2023-07-22 02:03:02 -0600

    cors options headers

--- server/src/index.js
+++ server/src/index.js
@@ -18,7 +18,14 @@ const corsOptions = {
   optionsSuccessStatus: 200,
 };
 
-app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+//app.use(cors(corsOptions));
 app.use(express.json());
 //app.use(urlencoded({ extended: true }));
 
