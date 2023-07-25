951c21cc035e7145ca593e040b2e7ae6f22840c3
Edgarcarrog <56626783+Edgarcarrog@users.noreply.github.com>
2023-07-25 14:14:41 -0600

    index test

--- server/index.js
+++ server/index.js
@@ -1,78 +1,31 @@
-require("dotenv").config();
 
 const express = require("express");
-const app = express();
+const { Router } = require("express");
 const cors = require("cors");
-const promisePool = require("./src/database/pool");
-const followRoutes = require("./src/v1/routes/follows.routes");
-const postRoutes = require("./src/v1/routes/posts.routes");
-const userRoutes = require("./src/v1/routes/users.routes");
-const { urlencoded } = require("express");
 
-//settings
-app.set("port", process.env.PORT || 4000);
+const app = express();
+const router = Router();
 
-//middlewares
 app.use(express.json());
-/* const corsOptions = {
-  origin: "*",
-  {
-    origin: [
-      "https://social-app-mysql-client.vercel.app",
-      "http://localhost:3000/",
-    ],
-    optionsSuccessStatus: 200,
-    credentials: true,
-  }
-}; */
-// app.options("/api/v1/users/verify-user/:user", cors());
+app.use(cors());
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
+router
+  .get("/", (req, res) => {
+    res.status(200).send("getting");
   })
-);
 
-/* app.use((req, res, next) => {
-   res.setHeader(
-     "Access-Control-Allow-Origin",
-     "https://social-app-mysql-client.vercel.app/"
-   );
-   res.append("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
-  res.append("Access-Control-Allow-Headers", "Content-Type");
-   next();
- }); */
+  .post("/", (req, res) => {
+    res.status(200).send("postting");
+  })
 
-//routers
-app.use("/api/v1", followRoutes);
-app.use("/api/v1", postRoutes);
-app.use("/api/v1", userRoutes);
+  .put("/", (req, res) => {
+    res.status(200).send("putting");
+  })
 
-try {
-  /* promisePool.query("DROP TABLE IF EXISTS users");
-  promisePool.query("DROP TABLE IF EXISTS posts");
-  promisePool.query("DROP TABLE IF EXISTS follows");
-   */
+  .delete("/", (req, res) => {
+    res.status(200).send("deleting");
+  });
 
-  promisePool.query("SET lc_time_names = 'es_ES';");
+app.use("/", router);
 
-  promisePool.query(
-    "CREATE TABLE IF NOT EXISTS users (userId VARCHAR(255) PRIMARY KEY, name VARCHAR(30) DEFAULT NULL, mail VARCHAR(50) DEFAULT NULL, password VARCHAR(255) DEFAULT NULL, birthday DATE DEFAULT NULL, email_verified BOOLEAN DEFAULT 0)"
-  );
-
-  promisePool.query(
-    "CREATE TABLE IF NOT EXISTS posts (Id VARCHAR(255) PRIMARY KEY, description VARCHAR(255), tags VARCHAR(255), likes INT DEFAULT 0, userId VARCHAR (255), rate DOUBLE(40,10), date DATETIME)"
-  );
-
-  promisePool.query(
-    "CREATE TABLE IF NOT EXISTS follows (Id VARCHAR(255) PRIMARY KEY, followerId VARCHAR(255), followingId VARCHAR(255))"
-  );
-
-  promisePool.query(
-    "CREATE TABLE IF NOT EXISTS likes_users (Id VARCHAR(255) PRIMARY KEY, postId VARCHAR(255), userId VARCHAR(255))"
-  );
-
-  app.listen(app.get("port"), console.log(`Server on port ${app.get("port")}`));
-} catch (error) {
-  console.log(error);
-}
+app.listen(5000, console.log("server on port 5000"));
