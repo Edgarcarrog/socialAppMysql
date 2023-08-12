e48fe7ee038a5afb09c52de0b8789252e06b4559
Edgarcarrog <edgc21@hotmail.com>
2023-08-12 04:17:49 -0600

    FEAT: ModalEditPost component implemented

--- client/src/pages/ProfilePage/index.js
+++ client/src/pages/ProfilePage/index.js
@@ -1,28 +1,29 @@
 import React, { useContext, useEffect, useState } from "react";
 import { context } from "../../context/context";
-import { Link } from "react-router-dom";
 import clienteAxios from "../../config/axios";
 import authToken from "../../helpers/authToken";
 import MyPosts from "../../components/MyPosts";
 import Following from "../../components/Following";
 import Modal from ".././../components/Modal/Modal";
+import ModalEditPost from ".././../components/ModalEditPost/ModalEditPost";
 
 const ProfilePage = () => {
   const {
     user,
     myposts,
     modal,
-    showModal,
     setMyPosts,
     followers,
     setFollowers,
     following,
     setFollowing,
+    showModal,
   } = useContext(context);
 
   const [display, setDisplay] = useState("myposts");
 
   useEffect(() => {
+    showModal(null);
     try {
       setPosts();
     } catch (error) {
@@ -48,8 +49,7 @@ const ProfilePage = () => {
     <>
       {modal && (
         <Modal>
-          <h2>Modal Perfil</h2>
-          <p>Chapa.</p>
+          <ModalEditPost />
         </Modal>
       )}
       <main className="container profile-container">
